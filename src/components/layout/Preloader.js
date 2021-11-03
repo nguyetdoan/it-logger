ad72b825bac66ad40e2a24b255abc50fc8871208
nguyetdoan <minhnguyetdoan057@gmail.com>
2021-11-03 11:39:18 +0700

    Add LogItem and Preloader

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <div class="progress blue lighten-4">
+      <div class="indeterminate blue"></div>
+    </div>
+  );
+};
+
+export default Preloader;
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import Preloader from "../layout/Preloader";
+import LogItem from "./LogItem";
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -18,16 +20,16 @@ const Logs = () => {
     setLoading(false);
   };
 
-  if (loading) return <h1>Loading...</h1>;
+  if (loading) return <Preloader />;
   return (
-    <ul className="collection-with-header">
+    <ul className="collection with-header">
       <li className="collection-header">
         <h4 className="center">System Logs</h4>
       </li>
       {logs.length === 0 ? (
         <p className="center">No log to show...</p>
       ) : (
-        logs.map((log) => <li>{log.message}</li>)
+        logs.map((log) => <LogItem key={log.id} log={log} />)
       )}
     </ul>
   );
